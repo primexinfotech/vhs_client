fc9b2ec026b096523b4f47cf61e9fdca82dac5bd
primexinfotech <primexinfotech94@gmail.com>
2025-07-13 09:40:43 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Josefin Sans']
+            }
+        },
+    },
+    plugins: [],
+}
